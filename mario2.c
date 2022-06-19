b9b7106afc84cb82ce4c75e59dd0c5752bf3a735
Dim0n90 <105513416+Dim0n90@users.noreply.github.com>
2022-06-19 14:45:34 +0300

    Add files via upload
    2 towers

---
+++ mario2.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+    h = get_int ("Vusota : "); 
+    }
+    while (h < 1 || h > 8);
+    for (int x = 0; x < h; x++)
+    {
+        for (int y = 0; y < h; y++)
+        {
+           if (x + y < h - 1) 
+           printf(" ");
+           else     
+           printf("#");
+        }   
+        printf(" ");
+        for (int y = 0; y < x + 1; y++)
+        {
+            printf ("#");
+        }
+        printf("\n");
+    }
+}
